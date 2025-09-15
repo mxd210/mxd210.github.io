fdbe617af2b3fbf0faf1d5a9cd597e7fd2d0bab1
mxd210 <mxd6686@gmail.com>
2025-09-15 10:50:11 +0700

    chore(sw): bump cache to v8

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-- // sw.js — MXD PWA v7
-- const VERSION = 'v7';
-+ // sw.js — MXD PWA v8
-+ const VERSION = 'v8';
+- // sw.js — MXD PWA v8
+- const VERSION = 'v8';
++ // sw.js — MXD PWA v9
++ const VERSION = 'v9';
 const CACHE_PREFIX = 'mxd';
 const CACHE = `${CACHE_PREFIX}-${VERSION}`;
 
