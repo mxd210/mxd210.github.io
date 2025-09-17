3bb48b1c71a7bb0fddda92b9cd67a74dc524a201
mxd210 <mxd6686@gmail.com>
2025-09-17 12:58:24 +0700

    git add assets/img/products/*.webp assets/products.js

---
+++ accsets/products.js
@@ -0,0 +1,258 @@
+// assets/products.js — generated from source list
+window.MXD_PRODUCTS = {
+  "1061097771-22861822077": {
+    "title": "Máy Mài Pin BOSCH 388VF không chổi than",
+    "price": 763240,
+    "list": 2255000
+  },
+  "300702715-5984515446": {
+    "title": "Máy cắt cầm tay Dekton DK-AG950S 950W",
+    "price": 822000,
+    "list": 920000
+  },
+  "654430280-22380739205": {
+    "title": "Máy Mài Pin HUKAN G1-AG6000J",
+    "price": 424000,
+    "list": 700000
+  },
+  "654430280-23055248675": {
+    "title": "Máy Khoan Đục Bê Tông Pin BOSHUN BS-R2803H",
+    "price": 814000,
+    "list": 1100000
+  },
+  "654430280-22255243106": {
+    "title": "Máy Mài Cầm Tay HUKAN HK-AG5030RP",
+    "price": 554000,
+    "list": 950000
+  },
+  "166287331-24663400599": {
+    "title": "Máy Mài Cầm Tay HUKAN HK-AG1909",
+    "price": 399000,
+    "list": 860000
+  },
+  "166287331-20882235158": {
+    "title": "Máy cắt gạch mini HUKAN HK-CG110B 1600W",
+    "price": 599000,
+    "list": 1690000
+  },
+  "1308044692-27757745334": {
+    "title": "Máy cắt gạch bàn BOSHUN BS-TS180 (lưỡi 180mm)",
+    "price": 1299000,
+    "list": 1999000
+  },
+  "319001815-18982725145": {
+    "title": "Máy cắt sắt 2T (350mm) lưỡi 14 inch",
+    "price": 1450000,
+    "list": 2290000
+  },
+  "319001815-23476218146": {
+    "title": "Máy cắt gạch để bàn 2T (lưỡi 110–125mm)",
+    "price": 1090000,
+    "list": 1690000
+  },
+  "91610174-17496283862": {
+    "title": "Máy cắt bê tông cầm tay KYNKO J1C-KD28-26",
+    "price": 1833000,
+    "list": 2490000
+  },
+  "718585685-40657720438": {
+    "title": "Máy cắt cỏ HUKAN 2 thì (tặng dây cước)",
+    "price": 990000,
+    "list": 1490000
+  },
+  "561921607-23854348138": {
+    "title": "Máy mài góc Dekton DK-AG9100 1000W",
+    "price": 759000,
+    "list": 1090000
+  },
+  "718585685-26903964352": {
+    "title": "Máy cắt gạch HUKAN HK-CG110A 1200W",
+    "price": 519000,
+    "list": 990000
+  },
+  "718585685-43811112520": {
+    "title": "Máy cắt gạch 3\" HUKAN (lưỡi 75mm)",
+    "price": 419000,
+    "list": 690000
+  },
+  "239636778-5965143102": {
+    "title": "Máy cắt gạch bàn mini 1/2HP",
+    "price": 890000,
+    "list": 1390000
+  },
+  "561921607-23754348735": {
+    "title": "Máy mài góc Dekton 950W DK-AG950S",
+    "price": 685000,
+    "list": 920000
+  },
+  "1307736249-26755400099": {
+    "title": "Máy mài để bàn 2 đá 150mm",
+    "price": 849000,
+    "list": 1290000
+  },
+  "1308044692-27655407437": {
+    "title": "Máy cưa bàn mini BOSHUN BS-TS4",
+    "price": 1590000,
+    "list": 2390000
+  },
+  "166287331-28552862620": {
+    "title": "Máy cắt gạch đa năng 1300W (kèm 2 lưỡi)",
+    "price": 649000,
+    "list": 1190000
+  },
+  "225431323-28409900154": {
+    "title": "Máy cắt gạch để bàn 750W",
+    "price": 789000,
+    "list": 1290000
+  },
+  "225431323-42155531660": {
+    "title": "Máy cắt gạch 1.2kW lưỡi 110mm",
+    "price": 719000,
+    "list": 1090000
+  },
+  "225431323-27084905616": {
+    "title": "Máy mài cầm tay 720W 100mm",
+    "price": 379000,
+    "list": 699000
+  },
+  "300702715-2924599192": {
+    "title": "Máy cắt gạch mini TOPKINT 1200W",
+    "price": 449000,
+    "list": 990000
+  },
+  "329099148-22371315416": {
+    "title": "Máy mài góc 100mm 720W (kèm đá 100)",
+    "price": 399000,
+    "list": 790000
+  },
+  "69531031-1579917792": {
+    "title": "Máy cắt gạch 110mm 1300W",
+    "price": 499000,
+    "list": 1090000
+  },
+  "1242691774-29165865471": {
+    "title": "Máy mài góc BOSHUN BS-G1001 1100W",
+    "price": 589000,
+    "list": 1090000
+  },
+  "217851417-16738318098": {
+    "title": "Máy mài góc HUKAN 2T",
+    "price": 399000,
+    "list": 699000
+  },
+  "300702715-21226248062": {
+    "title": "Máy cắt gạch TOPKINT 1600W",
+    "price": 599000,
+    "list": 1490000
+  },
+  "69531031-17781224066": {
+    "title": "Máy cắt gạch 125mm 1600W",
+    "price": 699000,
+    "list": 1390000
+  },
+  "1146804112-24706546657": {
+    "title": "Máy mài góc 100mm 900W (điều tốc)",
+    "price": 429000,
+    "list": 690000
+  },
+  "410556208-24223786596": {
+    "title": "Máy mài góc 850W 100mm",
+    "price": 399000,
+    "list": 590000
+  },
+  "75810249-14700002665": {
+    "title": "Máy cắt xích mini 4 inch (pin 21V)",
+    "price": 499000,
+    "list": 890000
+  },
+  "109816675-11061626170": {
+    "title": "Máy cắt cỏ 2 thì 43cc",
+    "price": 1080000,
+    "list": 1490000
+  },
+  "109816675-40357939565": {
+    "title": "Máy cắt cỏ 2 thì 52cc",
+    "price": 1190000,
+    "list": 1690000
+  },
+  "1022471396-22079403365": {
+    "title": "Máy cưa đĩa cầm tay 1600W lưỡi 185mm",
+    "price": 1089000,
+    "list": 1690000
+  },
+  "750843606-25883750845": {
+    "title": "Máy cưa đĩa mini 500W lưỡi 85mm",
+    "price": 509000,
+    "list": 890000
+  },
+  "750843606-18432109478": {
+    "title": "Máy cưa lọng 800W (kèm lưỡi)",
+    "price": 489000,
+    "list": 890000
+  },
+  "772892494-43960877125": {
+    "title": "Máy cắt bê tông KYNKO J1C-KD28-26 (phiên bản 1400W)",
+    "price": 2090000,
+    "list": 2690000
+  },
+  "772892494-20507810746": {
+    "title": "Máy khoan bê tông KYNKO 800W J1Z-KD10-10",
+    "price": 819000,
+    "list": 1290000
+  },
+  "772892494-21507014059": {
+    "title": "Máy khoan bê tông KYNKO J1C-KD28-26",
+    "price": 1833000,
+    "list": 2490000
+  },
+  "772892494-21507006756": {
+    "title": "Máy khoan động lực KYNKO 710W J1Z-KD10-10",
+    "price": 619000,
+    "list": 990000
+  },
+  "477191566-23116652705": {
+    "title": "Máy khoan pin 26V kèm phụ kiện",
+    "price": 379000,
+    "list": 799000
+  },
+  "1175273312-29976738485": {
+    "title": "Máy khoan pin 2 pin 36V (tặng mũi)",
+    "price": 599000,
+    "list": 990000
+  },
+  "189865177-19430598393": {
+    "title": "Máy khoan bê tông 850W 26mm",
+    "price": 849000,
+    "list": 1290000
+  },
+  "1349676167-42807970281": {
+    "title": "Máy cưa bàn mini 2000W",
+    "price": 1690000,
+    "list": 2590000
+  },
+  "1349676167-41554042244": {
+    "title": "Máy cưa bàn 10 inch 1800W",
+    "price": 2290000,
+    "list": 3290000
+  },
+  "7239997-25784321039": {
+    "title": "Máy cưa đĩa 1600W lưỡi 185mm",
+    "price": 949000,
+    "list": 1590000
+  },
+  "68653843-8580641349": {
+    "title": "Máy mài cắt đa năng 1600W",
+    "price": 699000,
+    "list": 1290000
+  },
+  "68653843-13507058015": {
+    "title": "Máy cắt gạch 1200W 110mm",
+    "price": 519000,
+    "list": 990000
+  },
+  "1061097771-22861822077": {
+    "title": "Máy Mài Pin BOSCH 388VF không chổi than",
+    "price": 763240,
+    "list": 2255000
+  }
+};
